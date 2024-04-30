8f27246e9e2f5d1c3cd114aeb10306376ebf28da
staldr <stlr@zhaw.ch>
2024-04-30 15:30:48 +0000

    Version A

--- src/app/gui-model/guimodel.ts
+++ src/app/gui-model/guimodel.ts
@@ -6,7 +6,7 @@ export class GuiModel {
 
     private _guiModel = {
         "application": {
-            "title": "ZHAW Requirements Engineering Friend Tracker (FS2024)",
+            "title": "STLR RE Friend Tracker (FS2024)",
             "formList": [
                 {
                     "id": "FriendForm",
